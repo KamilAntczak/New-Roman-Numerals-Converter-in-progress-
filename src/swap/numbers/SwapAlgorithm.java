d9056ecc73fd4d40fdfc33d1beed6f2770d82c08
Kamil Antczak <magnusik22@gmail.com>
2018-04-16 21:48:45 +0200

    swapToRoman function works fine. All cases are prepared

--- src/swap/numbers/SwapAlgorithm.java
+++ src/swap/numbers/SwapAlgorithm.java
@@ -20,8 +20,6 @@ public class SwapAlgorithm {
      private String giveRomanNumber(int number){
         switch(number){
             case 1: return "I";
-            case 2: return "II";
-            case 3: return "III";
             case 5: return "V";
             case 10: return "X";
             case 50: return "L";
@@ -36,62 +34,88 @@ public class SwapAlgorithm {
     }
     
     
-    private void swapAndAdd(int singleDigit, int power){
+    private void swapToRoman(int singleDigit, int power){
         int h;
         int r;
+        String smallerRomanNumber="";
+        String greaterRomanNumber;
         
         switch (singleDigit){
+            case 0:
+                break;
             case 1:
+                r = (int)(pow(10,power-1));
+                for(int i=0; i < singleDigit ; i++){
+                    smallerRomanNumber += giveRomanNumber(r);
+                }                              
+                System.out.println(smallerRomanNumber);
+                break;
             case 2:
+                r = (int)(pow(10,power-1));
+                for(int i=0; i < singleDigit ; i++){
+                    smallerRomanNumber += giveRomanNumber(r);
+                }                              
+                System.out.println(smallerRomanNumber);
+                break;
             case 3:
+                r = (int)(pow(10,power-1));
+                for(int i=0; i < singleDigit ; i++){
+                    smallerRomanNumber += giveRomanNumber(r);
+                }                              
+                System.out.println(smallerRomanNumber);
+                break;
             case 4:{
-                 h = (int) (pow(10,power)/2);
-                 r = (int) h - (int)((pow(10,power-1))*singleDigit);
-                System.out.println(h+" " + r);
-                String small= giveRomanNumber(r);
-                String big = giveRomanNumber(h);                              
-                System.out.println(small+"="+big);
+                h = (int)(pow(10,power)/2);
+                r = (int) h - (int)((pow(10,power-1))*singleDigit);
+                System.out.println(h+ " " + r);
+                smallerRomanNumber= giveRomanNumber(r);
+                greaterRomanNumber = giveRomanNumber(h);                              
+                System.out.println(smallerRomanNumber+greaterRomanNumber);
                 break;
             }           
             case 5:{
-                 r = (int) ((pow(10,power-1))*singleDigit);
+                 r = (int)((pow(10,power-1))*singleDigit);
                 String number = giveRomanNumber(r);
                 System.out.println(number);
                 break;
             }
             case 6:{
-                r = (int)(int) (pow(10,power)/2);
+                r = (int)(pow(10,power)/2);
                 h = (int)((pow(10,power-1))*singleDigit) - (int) (pow(10,power)/2);              
                 System.out.println(h+" " + r);
-                String small= giveRomanNumber(r);
-                String big = giveRomanNumber(h);                              
-                System.out.println(small+"="+big);
+                smallerRomanNumber= giveRomanNumber(r);
+                greaterRomanNumber = giveRomanNumber(h);                              
+                System.out.println(smallerRomanNumber+greaterRomanNumber);
                 break;
             }
             case 7:{
-                r = (int)(int) (pow(10,power)/2);
-                h = (int)((pow(10,power-1))*singleDigit) - (int) (pow(10,power)/2);              
-                System.out.println(h+" " + r);
-                String small= giveRomanNumber(r);
-                String big = giveRomanNumber(h);                              
-                System.out.println(small+"="+big);
+                r = (int)(pow(10,power)/2);
+                h = (int)((pow(10,power-1))*singleDigit) - (int)(pow(10,power)/2);   
+                greaterRomanNumber = giveRomanNumber(r);
+                System.out.println(h + " " + r);
+                for(int i =0 ; i < (h/((int)pow(10,power-1))); i++)
+                    smallerRomanNumber += giveRomanNumber((h/h)*(int)pow(10,power-1));
+                
+                System.out.println(greaterRomanNumber+smallerRomanNumber);
                 break;
             }               
-            case 8:{
-                r = (int)(int) (pow(10,power)/2);
-                h = (int)((pow(10,power-1))*singleDigit) - (int) (pow(10,power)/2);              
-                System.out.println(h+" " + r);
-                String small= giveRomanNumber(r);
-                String big = giveRomanNumber(h);                              
-                System.out.println(small+"="+big);
+            case 8:{ 
+                r = (int)(pow(10,power)/2);
+                h = (int)((pow(10,power-1))*singleDigit) - (int)(pow(10,power)/2);   
+                greaterRomanNumber = giveRomanNumber(r);
+                System.out.println(h + " " + r);
+                for(int i =0 ; i < (h/((int)pow(10,power-1))); i++)
+                    smallerRomanNumber += giveRomanNumber((h/h)*(int)pow(10,power-1));
+                
+                System.out.println(greaterRomanNumber+smallerRomanNumber);
                 break;
             }
             case 9:{
-                 h = (int) (pow(10,power));
-                 r = (int) h - (int)(pow(10,power-1));
-                String small= giveRomanNumber(h-r);
-                String big = giveRomanNumber(h);                              
-                System.out.println(small+"-"+big);
+                h = (int) (pow(10,power));
+                r = (int) h - (int)(pow(10,power-1));
+                smallerRomanNumber= giveRomanNumber(h-r);
+                greaterRomanNumber = giveRomanNumber(h);                              
+                System.out.println(smallerRomanNumber+greaterRomanNumber);
                 break;
             }      
         }
@@ -105,12 +129,11 @@ public class SwapAlgorithm {
         
         for(int i = 0; i < length; i++)
         {
-            int singleDigit = (int) (number / pow(10, length-1));
+            int singleDigit = (int) (number / pow(10, length-(1+i)));
             System.out.println(singleDigit);
-            swapAndAdd(singleDigit,length);
-        }
-        
-        
+            swapToRoman(singleDigit,length-i);   
+            number -= singleDigit*((int)pow(10, length-(1+i)));
+        }        
         return null;
     }
     
